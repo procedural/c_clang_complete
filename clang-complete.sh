ea6017458d76929684a7aba203f9c198fb1b1dd1
Constantine Tarasenkov <constantine@protonmail.com>
2016-02-07 05:11:42 +0300

    Support C++ namespaces

--- clang-complete.sh
+++ clang-complete.sh
@@ -11,8 +11,8 @@ fi
 IFS='%'
 str_line=$(sed "${line}q; d" "${1}")
 str_line_half=$(printf ${str_line} | cut -d ${key} -f1)
-str_tail=$(printf ${str_line_half} | rev | cut -d '.' -f1 | cut -d '>' -f1 | cut -d '(' -f1 | cut -d ',' -f1 | cut -d ' ' -f1 | rev)
+str_tail=$(printf ${str_line_half} | rev | cut -d '.' -f1 | cut -d '>' -f1 | cut -d ':' -f1 | cut -d '(' -f1 | cut -d ',' -f1 | cut -d ' ' -f1 | rev)
 
 column=$(( ${#str_line_half} - ${#str_tail} + 1 ))
 
-clang "${@}" -fsyntax-only -Xclang -code-completion-macros -Xclang -code-completion-at="${1}":${line}:${column} | grep "^COMPLETION: ${str_tail}" | sed "s_^COMPLETION: __1 ; s_^.* : __1 ; s_#\]_ _1 ; s_\[#__g ; s_#\]__g ; s_<#__g ; s_#>__g"
+clang "${@}" -fsyntax-only -Xclang -code-completion-macros -Xclang -code-completion-at="${1}":${line}:${column} | grep "^COMPLETION: ${str_tail}" | sed "s_^COMPLETION: __1 ; s_^.* : __1 ; s_#\]_ _1 ; s_\[#__g ; s_#\]__g ; s_<#__g ; s_#>__g ; s_{#__g ; s_#}__g"
